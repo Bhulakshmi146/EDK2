66982eaf6e5454a0f11547471556c6244c86192d
Bhulakshmi146 <karpurapu.bhulakshmi@gmail.com>
2025-05-12 11:27:06 +0530

    Update Jenkins.sh

--- Jenkins.sh
+++ Jenkins.sh
@@ -4,6 +4,10 @@ echo "Build complete!"
 
 . $WORKSPACE/edksetup.sh
 echo "executed edksetup file"
+make -C $WORKSPACE/BaseTools
+
+export EDK_TOOLS_PATH=$WORKSPACE/BaseTools
+. $WORKSPACE/edksetup.sh BaseTools
 
 cd $WORKSPACE
 build >> Build.txt
